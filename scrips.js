b71249a544a8d7b553bc24cdab680f4813d253c2
Samuel <sdf.dias.faria@gmail.com>
2020-06-17 22:22:18 -0300

    Cria o Repositorio

---
+++ scrips.js
@@ -0,0 +1,21 @@
+let numero1 = window.prompt ("Digite o Primeiro Número")
+
+let numero2 = window.prompt ("Digite o Segundo Número")
+
+
+if (numero1 > numero2) {
+
+    alert (numero1 + " é o maior número")
+
+} 
+
+else if  (numero1 === numero2) {
+
+    alert ("Os dois números são iguais")
+}
+
+else {
+
+    alert (numero2 + " é o maior número")
+
+}
